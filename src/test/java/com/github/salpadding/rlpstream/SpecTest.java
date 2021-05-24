6f496c02889073c0ab264f5a9703a2ce396d423c
zhuyingjie <m6567fc@outlook.com>
2021-05-24 16:33:01 +0800

    add test cases

---
+++ src/test/java/com/github/salpadding/rlpstream/SpecTest.java
@@ -0,0 +1,13 @@
+package com.github.salpadding.rlpstream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class SpecTest {
+    @Test
+    public void test0() {
+
+    }
+}
---
+++ src/test/java/com/github/salpadding/rlpstream/TestUtil.java
@@ -0,0 +1,24 @@
+package com.github.salpadding.rlpstream;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.SneakyThrows;
+
+import java.io.InputStream;
+public class TestUtil {
+    public static ObjectMapper MAPPER = new ObjectMapper()
+        .enable(SerializationFeature.INDENT_OUTPUT)
+        .enable(JsonParser.Feature.ALLOW_COMMENTS);
+
+
+
+    @SneakyThrows
+    public static byte[] readClassPathFile(String name){
+        InputStream in = Util.class.getClassLoader().getResource(name).openStream();
+        byte[] all = new byte[in.available()];
+        if(in.read(all) != all.length)
+            throw new RuntimeException("read failed");
+        return all;
+    }
+}
